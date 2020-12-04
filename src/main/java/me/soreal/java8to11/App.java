cd68706d9c3e60c4566df9073a5c4a1e94ede74d
sojin.kim <sojin.kim@lemonhc.com>
2020-12-04 17:41:49 +0900

    자바 8 API 기본, 스태틱 메소드

--- src/main/java/me/soreal/java8to11/App.java
+++ src/main/java/me/soreal/java8to11/App.java
@@ -1,7 +1,7 @@
 package me.soreal.java8to11;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.sql.SQLOutput;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
@@ -41,6 +41,46 @@ public class App {
         bar.printName(); // soreal
 
         Bar.printAnything(); // 스태틱 메소드 호출  // Bar
+
+
+        // 자바 8 API 기본 메소드
+
+        // Iterable 기본 메소드
+        List<String> name  = new ArrayList<>();
+        name.add("soreal");
+        name.add("jiny");
+        name.add("jean");
+        name.add("sojin");
+
+        name.forEach(System.out::println);
+
+        System.out.println("============");
+        for (String n: name) {
+            System.out.println(n);
+        }
+
+        System.out.println("=============");
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit(); // 절반으로 쪼갬
+        while (spliterator.tryAdvance(System.out::println)); // jean, sojin
+        System.out.println("=============");
+        while (spliterator1.tryAdvance(System.out::println)); // soreal jiny
+
+        // Stream 예제
+        long k = name.stream().map(String::toUpperCase)
+                    .filter(s -> s.startsWith("S"))
+                    .count();
+        System.out.println(k);
+
+        name.removeIf(s -> s.startsWith("s"));
+
+        name.forEach(System.out::println); // jiny, jean
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed());
+
+
+
 
 
 
